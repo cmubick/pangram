f23d0af9d566aa307d289096513665cf0dbedcd0
Chris Ubick <chrisubick@gmail.com>
2021-01-03 14:24:46 -0800

    game play updates

--- site/src/pages/Dashboard/Dashboard.js
+++ site/src/pages/Dashboard/Dashboard.js
@@ -5,75 +5,228 @@ import {
 import styles from './Dashboard.module.css'
 import {
   getSession,
-  deleteSession 
+  deleteSession,
+  createGameLetters
 } from '../../utils'
+import PacmanLoader from "react-spinners/PacmanLoader";
+import { css } from "@emotion/core";
 
 class Dashboard extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {}
-
-    // Bindings
+    this.state = {
+      wordEntry: '',
+      loading: false,
+      answers: [],
+      message: '',
+      points: 0
+    }
     this.logout = this.logout.bind(this)
+    this.createGame = this.createGame.bind(this);
+    this.onEntryChange = this.onEntryChange.bind(this);
+    this.onBackClick = this.onBackClick.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onKeyPress = this.onKeyPress.bind(this);
   }
 
   async componentDidMount() {
-
     const userSession = getSession()
-
     this.setState({
       session: userSession,
-    })
+    }, () => {
+      this.createGame();
+    });
   }
 
-  /**
-   * Log user out by clearing cookie and redirecting
-   */
   logout() {
     deleteSession()
     this.props.history.push(`/`)
   }
 
-  render() {
+  createGame() {
+    this.setState({
+      loading: true
+    }, () => {
+      setTimeout(() => { // TODO: fix loading flag, then remove setTimeout hack
+        createGameLetters().then((data) => {
+          if (!data.sucess) {
+            this.createGame();
+          } else {
+            this.setState({
+              game: data,
+              loading: false,
+              wordEntry: '',
+              answers: [],
+              message: '',
+              points: 0
+            });
+          }
+        });
+      }, 10);
+    });
+  }
 
+  onKeyPress(event) {
+    console.log(event);
+    if (event.key === 'Enter') {
+      this.handleFormSubmit();
+    }
+  };
+
+  onEntryChange(letter) {
+    let currentWordEntry = this.state.wordEntry;
+    currentWordEntry += letter;
+    this.setState({
+      wordEntry: currentWordEntry,
+      message: ''
+    });
+  }
+
+  onBackClick() {
+    let currentWordEntry = this.state.wordEntry;
+    currentWordEntry = currentWordEntry.slice(0, -1);
+    this.setState({
+      wordEntry: currentWordEntry,
+      message: ''
+    });
+  }
+
+  onSubmit() {
+    let {wordEntry, game, answers, points, message } = this.state;
+    if (!wordEntry.includes(game.magicLetter)) {
+      this.setState({
+        message: `Must use the magic letter.`,
+        wordEntry: ''
+      })
+    } else if (wordEntry.length < 4) {
+      this.setState({
+        message: `Four letter minimum.`,
+        wordEntry: ''
+      })
+    } else if (game.words.indexOf(wordEntry) >= 0) {
+      if (answers.indexOf(wordEntry) === -1) {
+        answers.push(wordEntry);
+        if (game.pangram.indexOf(wordEntry) >= 0) {
+          points = points + 20;
+          message = 'PANGRAM! +20 Points'
+        } else {
+          points = points + wordEntry.length;
+          message = `Great! +${wordEntry.length} Points`
+        }
+        this.setState({
+          message: message,
+          answers: answers,
+          points: points,
+          wordEntry: ''
+        })
+      } else {
+        this.setState({
+          message: `Already found.`,
+          wordEntry: ''
+        })
+      }
+    } else {
+      this.setState({
+        message: `Not in our list.`,
+        wordEntry: ''
+      })
+    }
+  }
+
+  render() {
+    const backButtonText = "back";
+    const override = css`
+      display: block;
+      margin: 0 auto;
+      border-color: #F8E71C;
+    `;
     return (
       <div className={`${styles.container} animateFadeIn`}>
         <div className={styles.containerInner}>
 
-          { /* Navigation */ }
-
           <div className={styles.navigationContainer}>
+            <div 
+              className={`link`}
+              onClick={this.createGame}>
+                New
+            </div>
+            <div className={`${styles.artwork} animateFlicker`}>
+              PANGRAM
+            </div>
             <div 
               className={`link`}>
                 { this.state.session ? this.state.session.userEmail : '' }
-              </div>
+            </div>
             <div 
               className={`link`}
               onClick={this.logout}>
                 logout
-              </div>
+            </div>
           </div>
 
-          { /* Content */ }
-
+          {this.state.loading ? 
           <div className={`${styles.contentContainer}`}>
-
-            <div className={`${styles.artwork} animateFlicker`}>
-              <img 
-                draggable='false'
-                src={'./fullstack-app-artwork.png'} 
-                alt='serverless-fullstack-application' 
-              />
+            <div>
+              <PacmanLoader color={'#F8E71C'} loading={this.state.loading} css={override} />
             </div>
+            <br/>
+            <div className={`${styles.loadingMessage}`}>...Loading</div>
+          </div> 
+          : this.state?.game ?
+            <div className={`${styles.contentContainer}`}>
+              <div className={`${styles.gameStats}`}>
+                <div className={`${styles.points}`}>{this.state.points} Points</div>
+              </div>
+              <div className={`${styles.wordEntryWrapper}`}>
+                <span className={`${styles.wordEntry}`}>{this.state.wordEntry.toUpperCase()}</span>
+                <button onClick={this.onBackClick} className={`${styles.backButton}`}>{backButtonText}</button>
+              </div>
 
-            <div className={`${styles.welcomeMessage}`}>
-              Welcome to your serverless fullstack dashboard...
-            </div>
-
-          </div>
-          
-        </div>
+              <div>
+                <button onClick={() => this.onEntryChange(this.state.game.gameLetters[0])} className={`${styles.letterButton}`}>{this.state.game.gameLetters[0].toUpperCase()}</button>
+              </div>
+              <div className={`${styles.buttonWrapper}`}>
+                <button onClick={() => this.onEntryChange(this.state.game.gameLetters[1])} className={`${styles.letterButton}`}>{this.state.game.gameLetters[1].toUpperCase()}</button>
+                <span className={`${styles.buttonWrapper}`}></span>
+                <button onClick={() => this.onEntryChange(this.state.game.gameLetters[2])} className={`${styles.letterButton}`}>{this.state.game.gameLetters[2].toUpperCase()}</button>
+              </div>
+              <div className={`${styles.buttonWrapper}`}>
+                <button onClick={() => this.onEntryChange(this.state.game.magicLetter)} className={`${styles.magicLetterButton}`}>{this.state.game.magicLetter.toUpperCase()}</button>
+              </div>
+              <div className={`${styles.buttonWrapper}`}>
+                <button onClick={() => this.onEntryChange(this.state.game.gameLetters[3])} className={`${styles.letterButton}`}>{this.state.game.gameLetters[3].toUpperCase()}</button>
+                <span className={`${styles.buttonSpacer}`}></span>
+                <button onClick={() => this.onEntryChange(this.state.game.gameLetters[4])} className={`${styles.letterButton}`}>{this.state.game.gameLetters[4].toUpperCase()}</button>
+              </div>
+              <div className={`${styles.buttonWrapper}`}>
+                <button onClick={() => this.onEntryChange(this.state.game.gameLetters[5])} className={`${styles.letterButton}`}>{this.state.game.gameLetters[5].toUpperCase()}</button>
+              </div>
+              
+              <button onClick={this.onSubmit} className={`${styles.submitButton}`} onKeyPress={this.onKeyPress}>
+                SUBMIT
+              </button>
+              <div className={`${styles.errorWrapper}`}>
+                {this.state.message === '' ? null :
+                  <span className={`${styles.errorMessage}`}>{this.state.message}</span>
+                }
+              </div>
+              <div className={`${styles.gameStats}`}>
+                <div className={`${styles.points}`}>
+                  Words Found: <br/>
+                  {this.state.answers.map((answer, index) => {
+                    return (
+                      <span key={answer}>
+                        {answer}
+                        {this.state.game.pangram.indexOf(answer) >= 0 ?
+                          <span>(pangram!)</span> : null
+                        }<span hidden={this.state.answers.length < (index + 2)}>, </span>
+                      </span>
+                  )})}
+                </div>
+              </div>
+          </div> : null }
+        </div> 
       </div>
     )
   }
---
+++ site/src/utils/games.js
@@ -0,0 +1,90 @@
+import scrabble from 'scrabble';
+
+export const createGameLetters = async () => {
+  let response = {};
+  let gameLetters = getRandomLetters();
+  let numberOfTries = 1;
+  let magicLetter = gameLetters.charAt(Math.floor(Math.random() * gameLetters.length));
+  
+  while(!validateLettersArePlayable(gameLetters, magicLetter)) {
+    gameLetters = getRandomLetters();
+    magicLetter = gameLetters.charAt(Math.floor(Math.random() * gameLetters.length));
+    ++numberOfTries;
+  }
+  
+  let rawWords = scrabble(gameLetters + gameLetters + gameLetters + gameLetters + gameLetters + gameLetters);
+  let words = [];
+  let pangram = [];
+
+  for(let i = 0; i < rawWords.length; i++) {
+    if (rawWords[i].includes(magicLetter) && rawWords[i].length > 3) {
+      words.push(rawWords[i]);
+      if (rawWords[i].length === gameLetters.length) {
+        let regexString = constructRegex(gameLetters);
+        let regex = RegExp(regexString);
+        let response = regex.test(rawWords[i]);
+        if (response) {
+          pangram.push(rawWords[i]);
+        }
+      }
+    }
+  }
+
+  response.sucess = true;
+
+  if (words.length < 10 || pangram.length === 0) {
+    response.sucess = false;
+  }
+
+  response.pangram = pangram;
+  response.words = words;
+  response.gameLetters = gameLetters.replace(magicLetter, '');
+  response.magicLetter = magicLetter;
+  response.numberOfTries = numberOfTries;
+  console.log(response);
+  return response;
+}
+
+const getRandomLetters = () => {
+  let gameLetters = '';
+  let validCharacters = 'abcdefghijklmnopqrstuvwxyz';
+  let vowels = 'aeiou';
+  
+  // add a vowel
+  let char = vowels.charAt(Math.floor(Math.random() * vowels.length));
+  gameLetters += char;
+  validCharacters = validCharacters.replace(char, '');
+
+  // add 6 random unique letters
+  for ( var i = 0; i < 6; i++ ) {
+    char = validCharacters.charAt(Math.floor(Math.random() * validCharacters.length));
+    gameLetters += char;
+    validCharacters = validCharacters.replace(char, '');
+  }
+  return gameLetters;
+}
+
+const validateLettersArePlayable = (letters, magicLetter) => {
+  const words = scrabble(letters + letters + letters + letters + letters + letters);
+
+  if (words.length > 20) {
+    for(let i = 0; i < words.length; i++) {
+      let regexString = constructRegex(letters);
+      let regex = RegExp(regexString);
+      let response = regex.test(words[i]);
+      if (response && words[i].includes(magicLetter)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+const constructRegex = (letters) => {
+  let regexString = '';
+  for(let i = 0; i < letters.length; i++) {
+    regexString += `(?=.*${letters[i]})`;
+  }
+  return regexString;
+}
