585e7f82b06ddc7aa08bc3b8b53063b29934386e
Chris Ubick <chrisubick@gmail.com>
2021-01-03 14:28:57 -0800

    update gateway url in site

--- site/src/config.js
+++ site/src/config.js
@@ -13,6 +13,6 @@ config.domains = {}
  * This will enable your front-end to communicate with your back-end.
  * (e.g. 'https://api.mydomain.com' or 'https://091jafsl10.execute-api.us-east-1.amazonaws.com')
  */
-config.domains.api = 'https://dna9ljztti.execute-api.us-east-1.amazonaws.com'
+config.domains.api = 'https://rnokivm6v1.execute-api.us-east-1.amazonaws.com'
 
 export default config
